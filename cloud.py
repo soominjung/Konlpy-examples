476bcecc273ea12776361eccc4f44eed41650f34
soominjung <soomin@kist.re.kr>
2020-06-03 01:26:21 +0900

    modify cloud.py to exclude a main word from word clouds

--- cloud.py
+++ cloud.py
@@ -33,19 +33,21 @@ def read_wiki(keyword):
 
 def get_tags(wiki_text, ntags=50, multiplier=5):
     len_text = len(wiki_text)
-    if len_text>5000:
-        multiplier=1
+    if len_text>10000:
+        multiplier=4
+    elif len_text>5000:
+        multiplier=6
     elif len_text>3000:
-        multiplier=3
+        multiplier=8
     elif len_text>1000:
-        multiplier=5
+        multiplier=10
     else:
         multiplier=10
     
     nouns = Mecab().nouns(wiki_text)
     count = Counter(nouns)
     return [{ 'color': color(), 'tag': n, 'size': c*multiplier }\
-                for n, c in count.most_common(ntags)]
+                for n, c in count.most_common(ntags) if n != arg.keyword]
 
 def draw_cloud(tags, filename, fontname='Noto Sans CJK', size=(800, 600)):
     pytagcloud.create_tag_image(tags, filename, fontname=fontname, size=size)
